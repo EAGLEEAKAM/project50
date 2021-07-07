208894d8efd036106a560e4cac43539e7a7ff493
EAGLEEAKAM <77165496+EAGLEEAKAM@users.noreply.github.com>
2021-07-07 13:09:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function turnon() {
+    document.getElementById("bulb").src = "https://s3-projectsassets.whjr.online/On+Bulb.png";
+}
+
+function turnoff() {
+    document.getElementById("bulb").src = "https://s3-projectsassets.whjr.online/Off+Bulb.png";
+}
